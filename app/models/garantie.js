6bcd1f6c9f2b9e16d2b48423ad146a1775c74eb8
Unknown <valentin.magnant@orange.fr>
2018-03-26 22:54:18 +0200

    Garantie Update

--- app/models/garantie.js
+++ app/models/garantie.js
@@ -4,16 +4,35 @@ const Schema = mongoose.Schema;
 
 /** on déclare les schémas */
 
-const relationsGaranties = new Schema({
-    mini:{
-        garantie : {
-            type : String,
-        }
+const GarantieSchema = new Schema({
+    name:{
+        type: String,
+        required:[true, 'Vous devez indiquer un nom pour la garantie.']
+    },
+    resp_civile:{
+        type: Boolean,
+        required: [true, 'Responsabilité civile est à remplir.']
+    },
+    inc:{
+        type: Boolean,
+        required: [true, 'Ce champs doit être renseigné.']
+    },
+    defence_penal_recours:{
+        type: Boolean,
+        required: [true, 'Ce champs doit être renseigné.']
+    },
+    vol:{
+        type: Boolean,
+        required: [true, 'Ce champs doit être renseigné.']
+    },
+    dommage_tout_risque:{
+        type: Boolean,
+        required: [true, 'Ce champs doit être renseigné.']
     }
 });
 
-var DevisAuto = mongoose.model('DevisAuto', devisAutoSchema);
+var Garantie = mongoose.model('Garantie', GarantieSchema);
 
 module.exports = {
-    DevisAuto : DevisAuto
+    Garantie : Garantie
 };
